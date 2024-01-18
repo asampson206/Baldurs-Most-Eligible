8bd40d0fc44b4abe4331346ec0750dfa15bef354
alexander sampson <a.sampson206@gmail.com>
2024-01-17 20:29:11 -0800

    5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import {createBrowserRouter, RouteProvider} from 'react-router-dom'
 
 
 
 
 function App(){
-    const [users, setUsers] = useState([])
-    useEffect(() =>{
-        fetch("http://localhost:3001")
-          .then(r => r.json())
-          .then(data => setUsers(data))
-          .catch(error => console.error(error))
-      }, [])
+    
 
     return (
-        <div>
-        <h2>Welcome to Baldur's Most Eligible!</h2>
-        <h2>Several of my girlfriend's favorite's have been gathered</h2>
-        </div>
+        <>
+      <header>
+      
+      </header>
+      <main>
+        <h1>Welcome to Baldur's most Eligible!</h1>
+       
+      </main>
+    </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 
 
@@ -14,4 +15,5 @@ import App from './App'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
 root.render(<App />);
