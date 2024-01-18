be235cdb7b1193da1a4a7d12d3d043be238b4e39
alexander sampson <a.sampson206@gmail.com>
2024-01-17 19:19:01 -0800

    3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 import { useState, useEffect } from 'react';
 import {createBrowserRouter, RouteProvider} from 'react-router-dom'
 
 
 
 
+function App(){
+    return (
+        <div>
+        <h2>Welcome to Baldur's Most Eligible!</h2>
+        <h2>Several of my girlfriend's favorite's have been gathered</h2>
+        </div>
+    )
+}
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App
 
