d72761b956355b1a318dc75beb7c703c9506774b
alexander sampson <a.sampson206@gmail.com>
2024-01-19 18:38:54 -0800

    reordered navlinks

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,27 +3,37 @@ import { NavLink } from "react-router-dom";
 // import "./NavBar.css";
 
 function Navbar(){
+  const linkStyle = {
+    margin: "1rem",
+    color: 'black',
+    display: "block"
+    
+  };
 return(
     <nav className="navbar">
     <NavLink
         to="/"
-        className="nav-link">
+        className="nav-link"
+        style={linkStyle}>
         Home
       </NavLink>
       
       <NavLink
         to="/Astarion"
-        className="nav-link" >
+        className="nav-link"
+        style={linkStyle} >
         Astarion
       </NavLink>
       <NavLink
         to="/Shadowheart"
-        className="nav-link" >
+        className="nav-link"
+        style={linkStyle} >
         Shadowheart
       </NavLink>
       <NavLink
         to="/Karlach"
-        className="nav-link" >
+        className="nav-link" 
+        style={linkStyle}>
         Karlach
       </NavLink>
 
