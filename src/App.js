2a5f41a58d010fd36ed1a15d377fe8fe5fc0b24e
alexander sampson <a.sampson206@gmail.com>
2024-01-21 19:27:25 -0800

    concising

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import Navbar from './Navbar';
-import Shadowheart from './Shadowheart';
-import Karlach from './Karlach';
-import Astarion from './Astarion';
 import "./App.css"
 import CommentSection from './CommentSection';
 
