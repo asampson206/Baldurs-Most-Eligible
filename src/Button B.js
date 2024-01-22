5de7e7815ae8100a1a0b86c5d191afab2989daa1
alexander sampson <a.sampson206@gmail.com>
2024-01-21 19:32:38 -0800

    cleaning up button B

--- src/Button B.js
+++ src/Button B.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 
 function Button() {
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState('')
 
   useEffect(() => {
     fetch('http://localhost:3000/ships')
       .then(response => response.json())
       .then(data => {
-        setResult(data[0].Result);
+        setResult(data[0].Result)
       })
       .catch(error => {
-        console.error('Error:', error);
-      });
-  }, []);
+        console.error('Error:', error)
+      })
+  }, [])
 
   const handleButtonClick = () => {
-    alert(result);
-  };
+    alert(result)
+  }
 
   return (
     <div>
